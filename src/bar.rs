ee48c1bb64941444e77489302ec654acfc187a53
Heiligetomate <heiligetomate@protonmail.com>
2026-05-05 13:25:27 +0200

    basics

--- src/bar.rs
+++ src/bar.rs
@@ -1,4 +1,10 @@
-use crossterm::style::Color;
+use std::io::{Result, Write, stdout};
+
+use crossterm::{
+    ExecutableCommand, QueueableCommand, cursor,
+    style::{Color, Print, ResetColor, SetForegroundColor},
+    terminal,
+};
 
 const DEFAULT_CHAR: char = '▇';
 const DEFAULT_LENGTH: u16 = 30;
@@ -20,5 +26,41 @@ impl ProgressBar {
             complete_color: DEFAULT_COMPLETE_COLOR,
             incomplete_color: DEFAULT_INCOMPLETE_COLOR,
         }
+    }
+
+    fn generate_bar(&self, len: u16) -> String {
+        let mut res = String::new();
+        for _ in 0..len {
+            res.push(self.bar_char);
+        }
+        res
+    }
+
+    fn output(&self, progress: u16) -> Result<()> {
+        let mut stdout = stdout();
+
+        stdout
+            .queue(cursor::Hide)?
+            .queue(SetForegroundColor(self.complete_color))?
+            .queue(Print(self.generate_bar(progress)))?
+            .queue(SetForegroundColor(self.incomplete_color))?
+            .queue(Print(self.generate_bar(self.length - progress)))?
+            .queue(ResetColor)?
+            .queue(terminal::Clear(terminal::ClearType::FromCursorDown))?
+            .queue(cursor::Show)?;
+
+        stdout.flush()?;
+
+        Ok(())
+    }
+}
+
+pub trait GetBar {
+    fn get_bar(&self) -> &ProgressBar;
+}
+
+pub trait OutputBar: GetBar {
+    fn output(&self) -> Result<()> {
+        self.get_bar().output(5)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,26 @@
-fn main() {}
+use progress_bar::bar::{GetBar, OutputBar, ProgressBar};
+
+struct MyTestStruct {
+    val: u64,
+    progress_bar: ProgressBar,
+}
+
+impl MyTestStruct {
+    fn new(val: u64) -> Self {
+        let progress_bar = ProgressBar::default();
+        Self { val, progress_bar }
+    }
+}
+
+impl GetBar for MyTestStruct {
+    fn get_bar(&self) -> &ProgressBar {
+        &self.progress_bar
+    }
+}
+
+impl OutputBar for MyTestStruct {}
+
+fn main() -> std::io::Result<()> {
+    let foo = MyTestStruct::new(42);
+    foo.output()
+}
